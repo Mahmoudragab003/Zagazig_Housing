ce7b678bcd9da57a7573e03acfd8b742a9007e83
Mahmoudragab003 <Mahmoudragab003@users.noreply.github.com>
2025-12-17 23:49:15 +0000

    deploy: 2e79933558ba85c51232f1bdd215ed759b59cc5d

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Zagazig_Housing/sw.js', { scope: '/Zagazig_Housing/' })})}
